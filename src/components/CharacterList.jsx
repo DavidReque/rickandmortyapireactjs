6f64de50bc17d37ad86a0b501f4d40efe2188b75
David Requeno <davidrequeno52@gmail.com>
2023-03-30 18:14:09 -0600

    Page component

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,30 +1,7 @@
 import { useState, useEffect } from "react";
 import Character from "./Character";
 import Seacrh from "./Seacrh";
-
-function Page({ page, setPage }) {
-  return (
-    <div>
-      <p>Page: {page}</p>
-      <header className="d-flex justify-content-between align-items-center">
-        {page > 1 && (
-          <button
-            className="btn btn-primary btn-sm"
-            onClick={() => setPage(page - 1)}
-          >
-            Previous
-          </button>
-        )}
-        <button
-          className="btn btn-primary btn-sm"
-          onClick={() => setPage(page + 1)}
-        >
-          Next
-        </button>
-      </header>
-    </div>
-  );
-}
+import Page from "./Page";
 
 function CharacterList() {
   const [personajes, setPersonajes] = useState([]);
